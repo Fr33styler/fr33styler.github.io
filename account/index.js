bcd5ea477f2f25881659b46fa452c085a9ec705e
Fr33styler <Fr33stylerMC@gmail.com>
2026-05-11 22:16:13 +0300

    Wrapped fetch in a async method

--- account/index.js
+++ account/index.js
@@ -1,8 +1,12 @@
-const renewToken = await fetch("https://cdn.fr33styler.ro:8443/auth/accounts/token", {
-  credentials: "include",
-  method: "POST",
-});
+async function checkToken() {
+  const renewToken = await fetch("https://cdn.fr33styler.ro:8443/auth/accounts/token", {
+    credentials: "include",
+    method: "POST",
+  });
 
-if (!renewToken.ok) {
+  return renewToken.ok;
+}
+
+if (!checkToken()) {
   window.location.replace('../login');
 }

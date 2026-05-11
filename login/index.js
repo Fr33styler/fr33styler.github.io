d356df439a6c1f9d075df9834a2f8f69ab2ce949
Fr33styler <Fr33stylerMC@gmail.com>
2026-05-11 21:17:34 +0300

    Cleaned up login a bit

--- login/index.js
+++ login/index.js
@@ -1,24 +1,20 @@
 async function login() {
   const username = document.getElementById("username");
   const password = document.getElementById("password");
-  try {
-    const response = await fetch("https://cdn.fr33styler.ro:8443/auth/accounts/token/" + username.value, {
-      credentials: "include",
-      method: "POST",
-      body: JSON.stringify({ password: password.value }),
-      headers: {
-        "Content-type": "application/json",
-      },
-    });
+  const response = await fetch("https://cdn.fr33styler.ro:8443/auth/accounts/token/" + username.value, {
+    credentials: "include",
+    method: "POST",
+    body: JSON.stringify({ password: password.value }),
+    headers: {
+      "Content-type": "application/json",
+    },
+  });
 
-    console.log(response.status);
-    if (response.ok) {
-      console.log("Login successful! " + await response.json());
-      window.location.replace('../account/');
-    } else {
-      console.log("Invalid login!");
-    }
-  } catch (error) {
-    console.log(error.message);
+  console.log(response.status);
+  if (response.ok) {
+    console.log("Login successful! " + await response.text());
+    window.location.replace('../account/');
+  } else {
+    console.log("Invalid login!");
   }
 }

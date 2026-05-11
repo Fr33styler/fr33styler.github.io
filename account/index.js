9020a48f27438e89f0a908e3d1565bf93624788c
Fr33styler <Fr33stylerMC@gmail.com>
2026-05-11 23:30:02 +0300

    Replaced {username} with the username from the cookie

--- account/index.js
+++ account/index.js
@@ -9,3 +9,17 @@ async function validateTokenOrRedirect() {
 }
 
 validateTokenOrRedirect();
+
+function getCookie(name) {
+  const cookies = document.cookie.split('; ');
+  for (let i = 0; i < cookies.length; i++) {
+    const [key, value] = cookies[i].split('=');
+    if (key === name) return value;
+  }
+  return "Stranger";
+}
+
+const box = document.getElementById("box");
+const boxTitle = box.querySelector(".h1");
+boxTitle.textContent = boxTitle.textContent.replace("{username}", getCookie("username"));
+
